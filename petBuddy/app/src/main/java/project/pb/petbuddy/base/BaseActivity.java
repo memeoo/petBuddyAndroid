c0e5d582040576ed4daa189429ca7ed73dfc6ad2
meme <memehsforever@gmail.com>
2015-05-16 12:32:46 +0900

    add base stuffs

---
+++ petBuddy/app/src/main/java/project/pb/petbuddy/base/BaseActivity.java
@@ -0,0 +1,30 @@
+package project.pb.petbuddy.base;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by meme on 15. 5. 16..
+ */
+public class BaseActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
---
+++ petBuddy/app/src/main/java/project/pb/petbuddy/base/petBuddyApplication.java
@@ -0,0 +1,10 @@
+package project.pb.petbuddy.base;
+
+import android.app.Application;
+
+/**
+ * Created by meme on 15. 5. 16..
+ */
+public class petBuddyApplication extends Application{
+
+}
